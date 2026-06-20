cf6c9a12a47c748258c6c2b5337d3be1da8e9a40
FTTM <fatemehalimirzaee14@gmail.com>
2026-06-20 11:50:06 +0330

    fetched the data

--- src/components/template/Homepage.jsx
+++ src/components/template/Homepage.jsx
@@ -3,10 +3,10 @@ import { useEffect } from "react";
 function Homepage() {
   useEffect(() => {
     fetch(
-      "https://api.coingecko.com/api/v3/coins/market?vs_currency=usd&order=market_cap_desc&per_page=20&page1&x_cg_demo_api_key=CG-JdkEKVciWpxtDK5ADdTzxwD1",
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&x_cg_demo_api_key=CG-JdkEKVciWpxtDK5ADdTzxwD1",
     )
-      .then((res) => res.json)
-      .then(json);
+      .then((res) => res.json())
+      .then((json)=>console.log(json));
   }, []);
 
   return <div>Home page</div>;
