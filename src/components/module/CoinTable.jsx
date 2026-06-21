ac1a96876cc8b530e88a7994136113c5a5f903b6
FTTM <fatemehalimirzaee14@gmail.com>
2026-06-21 12:31:11 +0330

    made the coins chart

--- src/components/module/CoinTable.jsx
+++ src/components/module/CoinTable.jsx
@@ -1,6 +1,43 @@
+import chartdown from "../../assets/chart-down.svg"
+import chartup from "../../assets/chart-up.svg"
+
+
 function CoinTable({ coins }) {
   console.log(coins);
-  return <div>CoinTable</div>;
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Coin</th>
+            <th>Name</th>
+            <th>Price</th>
+            <th>24h</th>
+            <th>Total volume</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {coins.map(coin=>( 
+            <tr key={coin.id}>
+              <th>
+                <div>
+                  <img src={coin.image} alt={coin.name} />
+                  <span>{coin.symbol.toUpperCase()}</span>
+                </div>
+              </th>
+              <th>{coin.name}</th>
+              <th>${coin.current_price.toLocaleString()}</th>
+              <th>{coin.price_change_percentage_24h.toFixed(2)}%</th>
+              <th>${coin.total_volume.toLocaleString()}</th>
+              <th><img src={coin.price_change_percentage_24h >0 ? chartup:chartdown} alt="" /></th>
+            </tr>
+          ))}
+            </tbody>
+       
+      </table>
+    </div>
+  );
 }
 
 export default CoinTable;
