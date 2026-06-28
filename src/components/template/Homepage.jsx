372d14471d968d1146ef1e8e36559bc5c1ce3dae
FTTM <fatemehalimirzaee14@gmail.com>
2026-06-28 12:52:54 +0330

    paginated the datas from url

--- src/components/template/Homepage.jsx
+++ src/components/template/Homepage.jsx
@@ -3,25 +3,26 @@ import CoinTable from "../module/CoinTable";
 import Pagination from "../module/Pagination";
 import { getApi } from "../../services/cryptoApi";
 
-
 function Homepage() {
   const [coins, setCoins] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
+    setLoading(true);
     const fetchApi = async () => {
-      const res = await fetch(getApi());
+      const res = await fetch(getApi(page));
       const json = await res.json();
       setCoins(json);
       setLoading(false);
     };
 
     fetchApi();
-  }, []);
+  }, [page]);
 
   return (
     <div>
-      <Pagination />
+      <Pagination page={page} setPage={setPage} />
       <CoinTable coins={coins} loading={loading} />
     </div>
   );
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,11 +1,8 @@
-const BASE_URL="https://api.coingecko.com/api/v3/";
-const key="CG-JdkEKVciWpxtDK5ADdTzxwD1";
+const BASE_URL = "https://api.coingecko.com/api/v3/";
+const key = "CG-JdkEKVciWpxtDK5ADdTzxwD1";
 
+const getApi = (page) => {
+  return `${BASE_URL}coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=${page}&x_cg_demo_api_key=${key}`;
+};
 
-
-const getApi=()=>{
-    return(`${BASE_URL}coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&x_cg_demo_api_key=${key}`);
-}   
-
-
-export {getApi}
+export { getApi };
