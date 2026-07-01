f39f8ad3662ddfc4ba0f838c309864d8b1c50328
FTTM <fatemehalimirzaee14@gmail.com>
2026-07-01 12:07:47 +0330

    added the search ability

--- src/components/module/Search.jsx
+++ src/components/module/Search.jsx
@@ -1,14 +1,42 @@
-function Search({currency, setCurrency}) {
+import { useEffect, useState } from "react";
+
+import { searchUrl } from "../../services/cryptoApi";
+
+function Search({ currency, setCurrency }) {
+  const [search, setSearch] = useState("");
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    if (!search) return;
+    const searchApi = async () => {
+      const res = await fetch(searchUrl(search));
+      const json = await res.json();
+      if (json.coins) setCoins(json.coins);
+    };
+    searchApi();
+  }, [search]);
+
   return (
     <div>
-        <input type="text" />
-        <select value={currency}  onChange={e=>{setCurrency(e.target.value)}}>
-            <option value="usd">USD</option>
-             <option value="eur">EUR</option>
-              <option value="jpy">JPY</option>
-        </select>
+      <input
+        type="text"
+        placeholder="Search"
+        value={search}
+        onChange={(e) => {
+          setSearch(e.target.value);
+        }}
+      />
+      <select
+        value={currency}
+        onChange={(e) => {
+          setCurrency(e.target.value);
+        }}>
+        <option value="usd">USD</option>
+        <option value="eur">EUR</option>
+        <option value="jpy">JPY</option>
+      </select>
     </div>
-  )
+  );
 }
 
-export default Search
+export default Search;
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -5,4 +5,8 @@ const getApi = (page, currency) => {
   return `${BASE_URL}coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=10&page=${page}&x_cg_demo_api_key=${key}`;
 };
 
-export { getApi };
+const searchUrl = (query) => {
+  return `${BASE_URL}search?query=${query}&x_cg_demo_api_key=${key}`;
+};
+
+export { getApi, searchUrl };
